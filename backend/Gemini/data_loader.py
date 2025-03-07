8b2b6844b5cb754dec35b63abd30c58df588aa82
brandonyee-cs <brandonyee.nyc@gmail.com>
2025-03-07 13:26:10 -0500

    Added some gemini code. Not yet done

--- backend/Gemini/data_loader.py
+++ backend/Gemini/data_loader.py
@@ -1,1 +1,41 @@
-#Temp
+from backend.Gemini.weather import WeatherAPI
+from backend.Gemini.web_scraper import WebScraper
+
+class DataLoader:
+    def __init__(self):
+        economic_API_key = ''
+    
+    def get_weather(self):
+        weather = WeatherAPI()
+        season = weather.get_season("America/New_York", "northern")
+        return season
+
+    def get_website_data(self, sbID : str):
+        urls = {
+            'FWR' : 'https://www.theflowerwallrentalco.com',
+            'GRAZE' : 'https://grazebrands.com/upper-crust#uppercrust-locations',
+            'NAOMIS' : 'https://www.naomiskosherpizzamenu.com/?utm_source=gbp',
+            'BANDH' : 'https://www.bandhplumbingandheating.info/servicesplumbing.html',
+            'AEROLA' : 'https://www.areolatattooing.org',
+            'ENOODLE' : 'https://www.enoodle.nyc',
+            'BTFLI' : 'https://bathtubrefinishinglongisland.com',
+            'MST' : 'https://www.mainstreettaiwanese.com',
+            'ANDA' : 'https://andacafeflushing.com/',
+            'ARONS' : 'https://www.aronskissenafarms.com',
+            'FARS' : 'https://flushingappliancerepairspecialists.com',
+            'OTNPP' : 'https://www.orderthenewpizzaprofessor.com/?utm_source=gbp',
+            'TSB' : 'https://www.thesandwichbar.getsauce.com/',
+            'AS' : 'http://www.amazingsavings.com/',
+            'ZFSTG' : 'https://zenfusioncuisinetogo.com'
+        }
+
+        url = urls[sbID]
+        scraper = WebScraper(delay=1.5)  # 1.5 seconds delay between requests
+        results = scraper.scrape_urls(url)
+        return results
+    
+    def get_economic_data(self):
+        pass
+
+    def get_small_business_data(self, name : str):
+        pass
--- backend/Gemini/gemini.py
+++ backend/Gemini/gemini.py
@@ -1,1 +1,35 @@
-#Temp
+from backend.Gemini.data_loader import DataLoader
+
+import google.generativeai as genai
+from typing import Optional
+import os
+from dotenv import load_dotenv
+
+class Gemini: # Constructor
+    def __init__(self) -> None:
+        load_dotenv()
+        self.api_key = os.getenv('GOOGLE_API_KEY')
+
+        self.model = genai.GenerativeModel('gemini-pro')
+
+        self.prompt = 'Data Not Found'
+
+
+    def get_prompt(self, sbID : str): # Mutator
+        data_loader = DataLoader()
+
+        self.prompt = f"Act as a agentic machine learning model. You will be given a small business's scraped website data, transaction and business information, local economic data, and weather data. Construct a supply chain from start to endfor the given small business (given the information provided).Please provide your response as a string with each industry in the supply chain separate by a comma. 
+        Attached is the data:
+        {data_loader.get_weather()}
+        {data_loader.get_website_data(sbID)}
+        {data_loader.get_economic_data()}
+        {data_loader.get_small_business_data(sbID)}"
+    
+    def get_supply_chain(self, sbID : str): # Accessor
+        self.get_prompt(sbID)
+
+        try:
+            response = self.model.generate_content(self.prompt)
+            return response.text
+        except Exception as e:
+            return f"Error generating response: {str(e)}"
